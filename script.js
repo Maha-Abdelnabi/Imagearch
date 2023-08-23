5d3dfdb788def73f8e1e2aa3c3bfa8bff5ef64b0
Maha Abdelnabi <mahaomar@gmail.com>
2023-08-23 10:37:59 -0400

    create js file

---
+++ script.js
@@ -0,0 +1,12 @@
+const key = "QclcZHO8A_bKGZPHqwS_3NYl4_yiYxy8D35XeM3OLI4";
+
+const formEl = document.querySelector("form");
+const searchInputEl = document.getElementById("search-input");
+const searchResultsEl = document.querySelector(".search-results");
+const showMoreButtonEl = document.getElementById("show-more-button");
+
+
+
+formEl.addEventListener("submit", () => {
+console.log("submitted")
+});
