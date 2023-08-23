7b274bad703d9121edb5a44b8df700fe4c908833
Maha Abdelnabi <mahaomar@gmail.com>
2023-08-23 10:55:28 -0400

    connect to api key

--- script.js
+++ script.js
@@ -6,11 +6,13 @@ const searchResultsEl = document.querySelector(".search-results");
 const showMoreButtonEl = document.getElementById("show-more-button");
 
 let inputData = "";
+let page = 1;
 
 function searchImages(){
 //get the value from the input field
 inputData = searchInputEl.value;
-console.log(inputData)
+ const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${key}`;
+console.log(url)
 
 }
 formEl.addEventListener("submit", (event) => {
