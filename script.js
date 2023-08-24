33b32a6fac8ea977a11055ec6248668f041142c7
Maha Abdelnabi <mahaomar@gmail.com>
2023-08-24 09:42:20 -0400

    make the result appear on page 1

--- script.js
+++ script.js
@@ -8,14 +8,23 @@ const showMoreButtonEl = document.getElementById("show-more-button");
 let inputData = "";
 let page = 1;
 
-function searchImages(){
+async function searchImages(){
 //get the value from the input field
 inputData = searchInputEl.value;
  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${key}`;
-console.log(url)
-
+ console.log(url);
+ const response = await fetch(url);
+//convert data to json file
+const data = await response.json();
+console.log(data)
+//make the page results appears empty,and based on the results fill the page with images
+ if (page === 1) {
+ searchResultsEl.innerHTML = "";
 }
+}
+
 formEl.addEventListener("submit", (event) => {
   event.preventDefault();
+  page = 1;
   searchImages();
 });
